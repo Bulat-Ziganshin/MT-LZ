bc1cdf214333ce13720cb18dd6d43e769774d9a2
bulatz <bulat.ziganshin@gmail.com>
2016-01-22 18:06:54 +0300

    RadixSort: sort by 64-bit key instead of 32-bit key + 32-bit data : 175 -> 290 MiB/s!

--- RadixSort.cpp
+++ RadixSort.cpp
@@ -17,7 +17,7 @@ void RadixSortPass (const Key *InKey, const Data *InData, Key *OutKey, Data *Out
     for (size_t i=0; i<size; i++)
         cnt[key(InKey[i])]++;
     t.Stop();  printf("%8.3lf", t.Elapsed());
-    
+
     // Inclusive scan of cnt[] => compute first output position for every bin
     for (size_t i=0,sum=0; i<SortBins; i++)
     {
@@ -25,19 +25,19 @@ void RadixSortPass (const Key *InKey, const Data *InData, Key *OutKey, Data *Out
         cnt[i] = sum;
         sum = tmp;
     }
-        
+
     // Fill OutKey & OutData
-    const size_t N = 16;
+    const size_t N = 8;
     Key TempKey[SortBins*N];  Data TempData[SortBins*N];
     for (size_t i=0; i<size; i++)
     {
         auto bin = key(InKey[i]);
         auto index = bin*N + cnt[bin]%N;
         TempKey [index] = InKey[i];
-        TempData[index] = InData[i];
+//        TempData[index] = InData[i];
         if (cnt[bin]%N == N-1)
         {
-            auto memcpy = [] (void* outp, void* inp, size_t size) 
+            auto memcpy = [] (void* outp, void* inp, size_t size)
             {
                 __m128i* out = (__m128i*) outp;
                 __m128i* in  = (__m128i*) inp;
@@ -48,7 +48,7 @@ void RadixSortPass (const Key *InKey, const Data *InData, Key *OutKey, Data *Out
             index -= N-1;
             auto out = cnt[bin] - (N-1);
             memcpy (OutKey +out, TempKey +index, N*sizeof(Key));
-            memcpy (OutData+out, TempData+index, N*sizeof(Data));
+//            memcpy (OutData+out, TempData+index, N*sizeof(Data));
         }
         cnt[bin]++;
     }
--- TestRadixSort.cpp
+++ TestRadixSort.cpp
@@ -4,45 +4,45 @@
 #include "timer.h"
 #include "RadixSort.cpp"
 
-const uint64_t DATASET = uint64_t(100)<<20;
-using Key = uint32_t;  using Data = uint32_t;
-
-template <int Bits>
-void BENCHMARK (Key *keys, Data *data, Key *outkeys, Data *outdata)
+template <int Bits, typename Key, typename Data>
+void BENCHMARK (Key *keys, Data *data, Key *outkeys, Data *outdata, size_t size)
 {
     Timer t;  double speed;
 
-    for (size_t i=0; i<DATASET; i++)
+    for (size_t i=0; i<size; i++)
         keys[i] = i*123456791;
-    printf("%2d: ", Bits, int(DATASET>>20));
+    printf("%2d: ", Bits, int(size>>20));
     t.Start();
-    RadixSortPass<Key,Data,0,Bits> (keys, data, outkeys, outdata, DATASET);
-    t.Stop();  speed = DATASET/(t.Elapsed()/1000);
+    RadixSortPass<Key,Data,0,Bits> (keys, data, outkeys, outdata, size);
+    t.Stop();  speed = size/(t.Elapsed()/1000);
     printf(" ms : %9.3lf ms = %7.3lf MB/s = %7.3lf MiB/s : %2d\n", t.Elapsed(), speed/1e6, speed/(1<<20), Bits);
 }
 
 int main()
 {
-    auto keys = new Key [DATASET],  outkeys = new Key [DATASET];
-    auto data = new Data[DATASET],  outdata = new Data[DATASET];
-    printf("Sorting %d MiB\n", int(DATASET>>20));
+    const uint64_t size = uint64_t(100)<<20;
+    using Key = uint64_t;  using Data = uint32_t;
+    auto keys = new Key [size],  outkeys = new Key [size];
+    auto data = new Data[size],  outdata = new Data[size];
 
-    BENCHMARK<4>(keys, data, outkeys, outdata);
-    BENCHMARK<5>(keys, data, outkeys, outdata);
-    BENCHMARK<6>(keys, data, outkeys, outdata);
-    BENCHMARK<7>(keys, data, outkeys, outdata);
-    BENCHMARK<8>(keys, data, outkeys, outdata);
-    BENCHMARK<9>(keys, data, outkeys, outdata);
-    BENCHMARK<10>(keys, data, outkeys, outdata);
-    BENCHMARK<11>(keys, data, outkeys, outdata);
-    BENCHMARK<12>(keys, data, outkeys, outdata);
-    BENCHMARK<13>(keys, data, outkeys, outdata);
-    BENCHMARK<14>(keys, data, outkeys, outdata);
-    BENCHMARK<15>(keys, data, outkeys, outdata);
-    BENCHMARK<16>(keys, data, outkeys, outdata);
-    BENCHMARK<17>(keys, data, outkeys, outdata);
-    BENCHMARK<18>(keys, data, outkeys, outdata);
-    BENCHMARK<19>(keys, data, outkeys, outdata);
+    printf("Sorting %d MiB\n", int(size>>20));
+    BENCHMARK<4>(keys, data, outkeys, outdata, size);
+    BENCHMARK<5>(keys, data, outkeys, outdata, size);
+    BENCHMARK<6>(keys, data, outkeys, outdata, size);
+    BENCHMARK<7>(keys, data, outkeys, outdata, size);
+    BENCHMARK<8>(keys, data, outkeys, outdata, size);
+    BENCHMARK<9>(keys, data, outkeys, outdata, size);
+    BENCHMARK<10>(keys, data, outkeys, outdata, size);
+    BENCHMARK<11>(keys, data, outkeys, outdata, size);
+    BENCHMARK<12>(keys, data, outkeys, outdata, size);
+    BENCHMARK<13>(keys, data, outkeys, outdata, size);
+    BENCHMARK<14>(keys, data, outkeys, outdata, size);
+    BENCHMARK<15>(keys, data, outkeys, outdata, size);
+    BENCHMARK<16>(keys, data, outkeys, outdata, size);
+    BENCHMARK<17>(keys, data, outkeys, outdata, size);
+    BENCHMARK<18>(keys, data, outkeys, outdata, size);
+    BENCHMARK<19>(keys, data, outkeys, outdata, size);
+
     delete[] keys; delete[] data; delete[] outkeys; delete[] outdata;
     return 0;
 }
