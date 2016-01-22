bf9e29b304b45fed789feb18f92049c1fd8850f0
bulatz <bulat.ziganshin@gmail.com>
2016-01-22 15:57:03 +0300

    RadixSort: fixed performance by allocating arrays only once

--- TestRadixSort.cpp
+++ TestRadixSort.cpp
@@ -5,14 +5,11 @@
 #include "RadixSort.cpp"
 
 const uint64_t DATASET = uint64_t(100)<<20;
+using Key = uint32_t;  using Data = uint32_t;
 
 template <int Bits>
-void BENCHMARK()
+void BENCHMARK (Key *keys, Data *data, Key *outkeys, Data *outdata)
 {
-    using Key = uint32_t;  using Data = uint32_t;
-    auto keys = new Key [DATASET],  outkeys = new Key [DATASET];
-    auto data = new Data[DATASET],  outdata = new Data[DATASET];
-    
     Timer t;  double speed;
 
     for (size_t i=0; i<DATASET; i++)
@@ -22,27 +19,30 @@ void BENCHMARK()
     RadixSortPass<Key,Data,0,Bits> (keys, data, outkeys, outdata, DATASET);
     t.Stop();  speed = DATASET/(t.Elapsed()/1000);
     printf(" ms : %9.3lf ms = %7.3lf MB/s = %7.3lf MiB/s : %2d\n", t.Elapsed(), speed/1e6, speed/(1<<20), Bits);
-    delete[] keys; delete[] data; delete[] outkeys; delete[] outdata;
 }
 
 int main()
 {
+    auto keys = new Key [DATASET],  outkeys = new Key [DATASET];
+    auto data = new Data[DATASET],  outdata = new Data[DATASET];
     printf("Sorting %d MiB\n", int(DATASET>>20));
-    BENCHMARK<4>();
-    BENCHMARK<5>();
-    BENCHMARK<6>();
-    BENCHMARK<7>();
-    BENCHMARK<8>();
-    BENCHMARK<9>();
-    BENCHMARK<10>();
-    BENCHMARK<11>();
-    BENCHMARK<12>();
-    BENCHMARK<13>();
-    BENCHMARK<14>();
-    BENCHMARK<15>();
-    BENCHMARK<16>();
-    BENCHMARK<17>();
-    BENCHMARK<18>();
-    BENCHMARK<19>();
+
+    BENCHMARK<4>(keys, data, outkeys, outdata);
+    BENCHMARK<5>(keys, data, outkeys, outdata);
+    BENCHMARK<6>(keys, data, outkeys, outdata);
+    BENCHMARK<7>(keys, data, outkeys, outdata);
+    BENCHMARK<8>(keys, data, outkeys, outdata);
+    BENCHMARK<9>(keys, data, outkeys, outdata);
+    BENCHMARK<10>(keys, data, outkeys, outdata);
+    BENCHMARK<11>(keys, data, outkeys, outdata);
+    BENCHMARK<12>(keys, data, outkeys, outdata);
+    BENCHMARK<13>(keys, data, outkeys, outdata);
+    BENCHMARK<14>(keys, data, outkeys, outdata);
+    BENCHMARK<15>(keys, data, outkeys, outdata);
+    BENCHMARK<16>(keys, data, outkeys, outdata);
+    BENCHMARK<17>(keys, data, outkeys, outdata);
+    BENCHMARK<18>(keys, data, outkeys, outdata);
+    BENCHMARK<19>(keys, data, outkeys, outdata);
+    delete[] keys; delete[] data; delete[] outkeys; delete[] outdata;
     return 0;
 }
