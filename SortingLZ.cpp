49cfbb7ddfa0306be081125c1421669921814d82
bulatz <bulat.ziganshin@gmail.com>
2016-01-27 01:48:42 +0300

    SortingLZ64: order=2..8 now supported

--- SortingLZ.cpp
+++ SortingLZ.cpp
@@ -23,15 +23,29 @@ int main (int argc, char**argv)
 
     Timer t;  double speed;
     t.Start();
+    uint64_t mask = (order==8? 0 : uint64_t(1)<<(order*8)) - 1,  high_4_bytes = uint64_t(-1) << 32;
     for (size_t i = 0; i < size; i++)
-        InKey[i] = *(uint32_t*)(buf+i) + (uint64_t(i)<<32);  // pack 4 bytes + position
+    {
+        uint64_t hash = order<=4? *(uint64_t*)(buf+i) << 32
+                                : (((*(uint64_t*)(buf+i)) & mask)*123456791) & high_4_bytes;
+        InKey[i] = hash | i;  // pack hash + position
+    }
     t.Stop();  speed = size/(t.Elapsed()/1000);
     printf("Fill:  %9.3lf ms = %7.3lf MB/s = %7.3lf MiB/s\n", t.Elapsed(), speed/1e6, speed/(1<<20));
 
     t.Start();
-    std::tie(InKey,OutKey)  =  RadixSort (InKey, OutKey, size, 0, order);
+    std::tie(InKey,OutKey)  =  RadixSort (InKey, OutKey, size, 4, std::min(order,4));
     t.Stop();  speed = size/(t.Elapsed()/1000);
     printf("Sort:  %9.3lf ms = %7.3lf MB/s = %7.3lf MiB/s\n", t.Elapsed(), speed/1e6, speed/(1<<20));
+
+    t.Start();
+    int sum = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        sum += buf[uint32_t(InKey[i])];
+    }
+    t.Stop();  speed = size/(t.Elapsed()/1000) + (sum==42?1e-9:0);
+    printf("Scan:  %9.3lf ms = %7.3lf MB/s = %7.3lf MiB/s\n", t.Elapsed(), speed/1e6, speed/(1<<20));
 
     return 0;
 }
